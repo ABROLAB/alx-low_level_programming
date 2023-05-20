b4d7f023c75d75fb4240d6fcc1dbc8707ada420c
Abdullahi Ibrahim <olaideibrahim36@gmail.com>
2023-05-19 20:40:22 -0700

    Low programming needs learning

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,27 @@
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+
+/**
+ * main - Peints the last digit of a randomly generated number
+ * and whether it is greater than 5, less than 6, or 0
+ *
+ * Return Always 0.
+ */
+int main(void)
+{
+	int a;
+
+	srand(time(0));
+	a = rand() - RAND_MAZ/2;
+
+	if ((a % 10) > 5)
+	{
+		printf("Last digit of %d and is greater than 5\n", a,a%10);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is 0\n", a,a%10);
+	}
+	return(0);	
+}
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - prints the alphabet
+ *
+ * Return Always 0 (Success)
+ */
+int main(void)
+{
+	char letr[26] ="abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i=0; i<26; i++)
+	{
+		putchar(letr(i));
+	}
+	putchar("\n");
+	return (0);
+}

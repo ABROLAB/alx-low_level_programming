056330ce19dbc6bae496110a888adc8aed523a37
Abdullahi Ibrahim <olaideibrahim36@gmail.com>
2023-05-19 21:10:21 -0700

    correcting update-i

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,13 +17,13 @@ int main(void)
 	{
 		printf("%d is positive\n", a);
 	}
-	else.if(a==0)
+	else if(a==0)
 	{
 		printf("%d is zero\n", a);
 	}
 	else
 	{
-		printf("%d is nagative\n" a);
+		printf("%d is nagative\n", a);
 	}
 	return(0);
 

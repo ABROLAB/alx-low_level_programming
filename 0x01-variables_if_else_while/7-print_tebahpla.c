78baf75e64ea0b832bb5cafbb74e61e2eefde33c
Abdullahi Ibrahim <olaideibrahim36@gmail.com>
2023-05-21 10:48:32 -0700

    new commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - print the alphabets in reverse
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ab;
+
+	for (ab = 'z'; ab >= 'a'; ab--)
+		putchar(le);
+
+	putchar('\n');
+
+	return (0);
+}

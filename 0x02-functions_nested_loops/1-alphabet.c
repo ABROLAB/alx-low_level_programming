038db57a37006967021fce365b1c6daa49e5bc29
Abdullahi Ibrahim <olaideibrahim36@gmail.com>
2023-05-23 08:34:46 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ * write function to print alphabet lowercase followed by new line
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char printAlphab = 'a';
+
+	while (printAlphab <= 'z')
+	{
+		putchar(printAlphab);
+
+		printAlphab++;
+	}
+	putchar('\n');
+
+	return (0);
+}

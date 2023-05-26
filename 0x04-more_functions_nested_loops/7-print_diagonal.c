c924e0bc51e6bfae4c5b2a5e303ffe0a9a34cd02
Abdullahi Ibrahim <olaideibrahim36@gmail.com>
2023-05-25 17:31:14 -0700

    Fizz-Buzz test” is an interview question designed to help filter out the 99.5% of programming job

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - printing lines diagonal
+ * @n: input variable
+ *
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+		_putchar('\n');
+	for (i = 0; j < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\n');
+		_putchar('\n');
+	}
+}
